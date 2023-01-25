db808fcd143b8d1d28acf65542ca9c63f3716bc9
João Lucas <joaolucaspweb@gmail.com>
2023-01-25 01:29:27 -0300

    Finalizado

--- Exercicios_faceis/1.twosum.py
+++ Exercicios_faceis/1.twosum.py
@@ -1,8 +1,3 @@
-# Descrição do exercicio
-# Given an array of integers nums and an integer target,
-# return indices of the two numbers such that they add up to target.
-
-
 def sumtwo(nums, target):
     index_index_auxiliar = 0
     index = []
