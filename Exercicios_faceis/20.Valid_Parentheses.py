2c7092d6aab4fd84c982d2bfe63aa2445ee1a764
João Lucas <joaolucaspweb@gmail.com>
2023-03-12 02:00:25 -0300

    Finalizado

--- Exercicios_faceis/20.Valid_Parentheses.py
+++ Exercicios_faceis/20.Valid_Parentheses.py
@@ -1,20 +1,23 @@
 #link do exercicio:
 #https://leetcode.com/problems/valid-parentheses/
-s = '[](){}'
+s = '() {} []'
 
 class Solution:
     def validaString(self, s: str) -> bool:
         self.s = s
         self.caracteres = ["()", "[]", "{}"]
 
-        for k in enumerate(self.s):
-            try:
-                if self.s[k[0]] + self.s[k[0] + 1] in self.caracteres:
-                    return True
-                else:
-                    return False
-            except:
-                pass
+        for k, v in enumerate(self.s):
+            if len(self.s) == 1:
+                return False
+            else:
+                try:
+                    if self.s[k] + self.s[k + 1] in self.caracteres:
+                        return True
+                    else:
+                        return False
+                except:
+                    pass
 
 
 solution = Solution()
